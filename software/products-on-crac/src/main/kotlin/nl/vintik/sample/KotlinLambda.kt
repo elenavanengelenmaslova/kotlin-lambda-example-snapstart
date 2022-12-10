66a8aa54d3fed0e418a3b9a8007b2f691437f2ad
elena.vanengelen <elena.vanengelen@edsn.nl>
2022-12-10 09:24:53 +0100

    Create snap start + alternatives

--- software/products-on-crac/src/main/kotlin/nl/vintik/sample/KotlinLambda.kt
+++ software/products-on-crac/src/main/kotlin/nl/vintik/sample/KotlinLambda.kt
@@ -4,9 +4,11 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import nl.vintik.sample.model.Product
 import nl.vintik.sample.model.Product.Companion.productTable
+import nl.vintik.sample.util.logger
 
 class KotlinLambda : RequestHandler<Map<String, String>, Product?> {
     override fun handleRequest(event: Map<String, String>, context: Context): Product? {
+        logger().info("SnapStart on CRaC")
         return ProductsController(ProductsService(productTable)).find("1")
     }
 }
