c2a9b3b7e6d6d6059e9de03b87aac4e080dd8d18
elena.vanengelen <elena.vanengelen@edsn.nl>
2022-12-11 13:06:06 +0100

    Fix priming

--- software/products-on-crac/src/main/kotlin/nl/vintik/sample/KotlinLambda.kt
+++ software/products-on-crac/src/main/kotlin/nl/vintik/sample/KotlinLambda.kt
@@ -6,9 +6,14 @@ import nl.vintik.sample.model.Product
 import nl.vintik.sample.model.Product.Companion.productTable
 import nl.vintik.sample.util.logger
 
+@Suppress("UNUSED")
 class KotlinLambda : RequestHandler<Map<String, String>, Product?> {
     override fun handleRequest(event: Map<String, String>, context: Context): Product? {
         logger().info("SnapStart on CRaC")
         return ProductsController(ProductsService(productTable)).find("1")
+    }
+    companion object {
+        @Suppress("UNUSED")
+        private val priming = PrimingResource()
     }
 }
