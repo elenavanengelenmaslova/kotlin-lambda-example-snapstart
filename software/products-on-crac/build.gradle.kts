684c9c85409cb6d66e6dfa78d435f8b163b4d288
elena.vanengelen <elena.vanengelen@edsn.nl>
2023-02-16 10:37:26 +0100

    upgrade dynamo sdk and add logging in priming readme

--- software/products-on-crac/build.gradle.kts
+++ software/products-on-crac/build.gradle.kts
@@ -1,6 +1,6 @@
 
 dependencies {
-    implementation("software.amazon.awssdk:dynamodb-enhanced:2.19.4")
+    implementation("software.amazon.awssdk:dynamodb-enhanced:2.20.3")
     implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
     implementation("com.amazonaws:aws-lambda-java-events:3.11.0")
     implementation("io.github.crac:org-crac:0.1.3")
--- software/products-on-crac/src/main/kotlin/nl/vintik/sample/KotlinLambda.kt
+++ software/products-on-crac/src/main/kotlin/nl/vintik/sample/KotlinLambda.kt
@@ -12,22 +12,26 @@ import org.crac.Resource
 @Suppress("UNUSED")
 class KotlinLambda : RequestHandler<ProductRequest, Product?>, Resource {
     private val productsController = ProductsController(ProductsService(productTable))
+
     init {
         //register priming
         Core.getGlobalContext().register(this)
     }
+
     override fun beforeCheckpoint(context: org.crac.Context<out Resource>?) {
-        //beforeCheckpoint hook
+        logger().info("beforeCheckpoint hook started")
         runCatching {
             productsController.find("i dont exist")
-        }
+        }.onFailure { logger().error(it.message) }
+        logger().info("beforeCheckpoint hook finished")
     }
+
     override fun handleRequest(event: ProductRequest, context: Context): Product? {
         logger().info("SnapStart and CRaC")
         return productsController.find(event.id)
     }
 
     override fun afterRestore(context: org.crac.Context<out Resource>?) {
-       //afterRestore hook
+        logger().info("afterRestore hook")
     }
 }
