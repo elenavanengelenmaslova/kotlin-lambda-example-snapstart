6ef0cd019fa37955d1fb74d451544a258593f4c9
elena.vanengelen <elena.vanengelen@edsn.nl>
2023-04-10 10:41:21 +0200

    upgrade versions

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 }
 
 dependencies {
-    implementation("software.amazon.awscdk:aws-cdk-lib:2.55.1")
-    implementation("software.constructs:constructs:10.1.194")
+    implementation("software.amazon.awscdk:aws-cdk-lib:2.69.0")
+    implementation("software.constructs:constructs:10.1.280")
 }
 
 application {
